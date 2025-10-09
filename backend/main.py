92b24bfe24e22bf901dcd569d0799f46c6529bae
Baye Yoro FALL <fallbayeyoro1@gmail.com>
2025-10-09 15:46:44 +0000

    modify document schemas and model

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,15 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.api.user import user_router
 from src.api.document import doc_router
-
 from src.database.database import init_db
 
+
 @asynccontextmanager
-async def life_span(app:FastAPI):
+async def life_span(app: FastAPI):
     print("Server start")
     await init_db()
     yield
-    print('Server is stoped')
+    print("Server is stoped")
 
 
 app = FastAPI(lifespan=life_span)
